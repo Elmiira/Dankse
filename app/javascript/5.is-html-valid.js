f8f4efd3d14472b92e5ab54da70525603ba82744
Elimira <khodaee.elmira@gmail.com>
2020-03-11 00:02:56 +0100

    feat: Complete partial html parser

--- app/javascript/5.is-html-valid.js
+++ app/javascript/5.is-html-valid.js
@@ -12,4 +12,32 @@
 // "<div>"     =>  False
 // "<div><div><span></span></div></h1>" =>  False
 
-function isHTMLValid(html) {}
+// <div> a </div> 
+
+export default function isHTMLValid(html) {
+  const regex = /<\/?[a-z-1-9]*>/gm;
+  const tagList = html.match(regex);
+  const startingTags = ['<div>', '<h1>', '<span>'];
+  const orderStack = [];
+  for (let index = 0; index < tagList.length; ++index) {
+    if (startingTags.includes(tagList[index])) {
+      orderStack.push(tagList[index]);
+    }
+    else if (tagList[index] == '</div>') {
+      if (orderStack.pop() !== '<div>') {
+        return false;
+      }
+    }
+    else if (tagList[index] == '</span>') {
+      if (orderStack.pop() !== '<span>') {
+        return false;
+      }
+    }
+    else if (tagList[index] == '</h1>') {
+      if (orderStack.pop() !== '<h1>') {
+        return false;
+      }
+    }
+  };
+  return orderStack.length == 0;
+}
